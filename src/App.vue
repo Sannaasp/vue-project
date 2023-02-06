5557ce5ede8bffc6f2244aaeca359443324eba57
Sanna Asp <sannaasp@AirsomthorSanna.lan>
2023-02-06 21:30:11 +0100

    first comit starting project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import CitiesList from "./components/CitiesList.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    CitiesList,
+    HelloWorld,
+  },
+  data() {
+    return {
+      message: "Kallekalas",
+    };
+  },
+};
+</script>
+
+<template>
+  <HelloWorld msg="Hello World!" />
+  <CitiesList />
+  <h1 class="black">Hej sanna, jag är en H1:a</h1>
+  <h2>{{ message }}</h2>
+  <input type="button" @click="onClick" value="Hej" />
+</template>
+
+<style>
+.black {
+  color: red;
+}
+</style>
---
+++ src/components/CitiesList.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  created() {
+    fetch("https://avancera.app/cities/")
+      .then((response) => response.json())
+      .then((cities) => {
+        this.cities = cities;
+      });
+  },
+  data() {
+    return {
+      cities: null,
+    };
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="city in cities">{{ city.name }}</li>
+  </ul>
+</template>
